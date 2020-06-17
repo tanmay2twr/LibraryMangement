a43e91e4738898cd45bcb4110edcbf548f87a25f
Tanmay Tiwari <tanmaytwr01@gmail.com>
2020-06-18 01:59:35 +0530

    First time commit

---
+++ LibraryMangement/src/com/wipro/Controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.wipro.Controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
